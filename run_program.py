735325ace69130f361819fb175b13802d2fb11e3
santiago larrain <sjlarrain@uc.cl>
2025-03-08 18:58:02 -0300

    Miss save

---
+++ run_program.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+import platform
+import sys
+
+# Get the full path to the install_requirements.sh script
+script_path_unix = os.path.join(os.path.dirname(__file__), 'install_requirements.sh')
+script_path_windows = os.path.join(os.path.dirname(__file__), 'install_requirements_windows.py')
+
+# Print the path of the script being executed
+if platform.system() == "Windows":
+    print(f"Executing script at: {script_path_windows}")
+    subprocess.run([sys.executable, script_path_windows], shell=True)
+else:
+    print(f"Executing script at: {script_path_unix}")
+    subprocess.run(["/bin/bash", script_path_unix], shell=True)
+
+# Run main.py after installing the requirements
+main_script_path = os.path.join(os.path.dirname(__file__), 'main.py')
+print(f"Executing main script at: {main_script_path}")
+subprocess.run([sys.executable, main_script_path], shell=True)
